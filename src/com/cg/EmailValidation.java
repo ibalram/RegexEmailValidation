9c0da7b2b69edcbbe530233b27eab37158ab8f36
ibalram <balramsinghrathore3@gmail.com>
2020-09-29 02:44:39 +0530

    Added: Validate first optional part

--- src/com/cg/EmailValidation.java
+++ src/com/cg/EmailValidation.java
@@ -10,16 +10,16 @@ public class EmailValidation {
 		System.out.println("Email validation program");
 		System.out.println("------------------------");
 		String email;
-		String emailPattern = "^[a-zA-Z0-9+_-]+@[a-zA-Z0-9]+([.][a-z]{2,}).*";
+		String emailPattern = "^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-z]{2,}).*";
 		boolean repeat = true;
-		while(repeat) {
+		while (repeat) {
 			System.out.println("Select option:\n1. Enter mail to check\n2. exit");
 			int option = Integer.parseInt(sc.nextLine());
-			switch(option) {
+			switch (option) {
 			case 1:
 				System.out.println("Enter Email Id:");
 				email = sc.nextLine();
-				if (Pattern.matches(emailPattern,email)) 
+				if (Pattern.matches(emailPattern, email))
 					System.out.println("Valid Email\n");
 				else
 					System.out.println("Invalid Email\n");
